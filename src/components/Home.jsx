600f5d17495ad90255dfce3a8eaf02addcc711c7
David Pecic <david-pecic@gmx.de>
2023-06-10 14:43:48 +0200

    changed spelling errorw of table component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
-import Table from './table';
+import Table from './Table';
 
 function Home() {
   const [errors, setErrors] = useState({})
